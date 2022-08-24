1ece8a3824dec2e89cfd331a5c0436255642fb67
Zenitsumll <111942515+Zenitsumll@users.noreply.github.com>
2022-08-24 04:57:49 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,46 +16,46 @@ global.APIKeys = {
 global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
+*Zenitsu Ξ*
+*I am 17 years old developer...*
+*From Egypt 🇪🇬*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
-global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
-global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
-global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
+*Follow My Tik Tok - https://vm.tiktok.com/ZMN7J8yWP/  // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+201022506066'] //👈  Enter Your number
+global.premium =  ['+201022506066'] //👈  Enter Your number
+global.ownernomer = '+201022506066' //👈  Enter Your number
+global.ownername = 'Zenitsu' //👈 Enter Your name
+global.botname = 'ZENITSU ＢＯＴ' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 ZENITSU 𝖡𝗈𝗍' //👈 Enter Your Name
+global.ig = 'https://github.com/Zenitsubot/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
+global.region = 'Egypt, Cairo, Ain shams ' // 👈 You Can change this your choice
+global.sc = 'https://github.com/Zenitsubot/GARFIELD-WHATSAPP-BOT-v8'
 global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
-global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
+global.myweb = 'https://vm.tiktok.com/ZMN7J8yWP/'// 👈 Enter your Social media link to follow now button
 global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
-global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
+global.bye = '*Bye bye 🐼*' // 👈 You Can change this your choice
+global.packname = 'Zenitsu-BOT'  // 👈 You Can change this your choice 
 global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
-global.thumb =  'https://i.ibb.co/Jz0mMnC/Pics-Art-22-06-03-01-01-46-876.png' 
-global.spoty = 'https://i.postimg.cc/HW2YbN1b/Pics-Art-22-08-09-16-12-14-489.png'
+global.thumb =  'https://ibb.co/tHsN7GP'
+global.spoty = 'https://ibb.co/qWhg5PP'
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
 global.sp = '⭔'
 global.mess = {
-    success: 'Done 🦋', // 👈 You Can change this your choice
-    admin: 'This Feature Is Only For Admin!', // 👈 You Can change this your choice
-    botAdmin: 'Bot Must Be Admin First!', // 👈 You Can change this your choice
-    owner: 'This Feature Is Only For Owner!', // 👈 You Can change this your choice
-    group: 'Feature Used Only For Groups!', // 👈 You Can change this your choice
-    private: 'Features Used Only For Private Chat!', // 👈 You Can change this your choice
-    bot: 'This Feature Is Only For Bot', // 👈 You Can change this your choice
-    wait: '```Please Wait...```', // 👈 You Can change this your choice
-    error: 'Error! Maybe Api Key Is Expired🤔!', // 👈 You Can change this your choice
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    success: 'تم 🦋', // 👈 You Can change this your choice
+    admin: 'هذه الميزة للمشرف فقط يغالي!', // 👈 You Can change this your choice
+    botAdmin: 'خليني ادمن الاول!', // 👈 You Can change this your choice
+    owner: 'هذه الميزة للمالك فقط (الي هو زينيتسو الفاجر)!', // 👈 You Can change this your choice
+    group: 'في الجروب بس يغالي!', // 👈 You Can change this your choice
+    private: 'في الخاص بس يغالس!', // 👈 You Can change this your choice
+    bot: 'للبوت بس يغالي اتقي الله', // 👈 You Can change this your choice
+    wait: '```Please Wait...```', // 👈 You Can change this your choice
+    error: 'Error! بطل تلعب🤔!', // 👈 You Can change this your choice
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 } // 👈 You Can change this your choice
 /// Do Not Change 👇 
 global.limitAwal = "99999999999999999"
@@ -71,18 +71,18 @@ global.HunterApi = "FuckBitch"
 global.neokey = "V8qnSaTq"
 global.riy = "RiyGanz"
 
-    global.limitawal = {
-    premium: "Infinity",
-    free: 12,
-    monayawal: 1000
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
 }
-   global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
 }
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
